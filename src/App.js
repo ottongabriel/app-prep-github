61ae20a12c6b4846092a1b5ab3b9555276851fd8
Ernesto Rodriguez <nestor@visionpoint.io>
2018-06-23 00:10:21 -0400

    Style changes to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+const styles = {
+  wrapper: {
+    display: 'flex',
+    height: 560,
+    background: 'linear-gradient(to bottom, #5a4ba3 0%, #7b3795 100%)',
+  },
+};
 
 class App extends Component {
   render() {
-    return <div className="App">Hey</div>;
+    const { classes } = this.props;
+
+    return (
+      <Fragment>
+        <CssBaseline />
+
+        <div className={classes.wrapper}>Hey</div>
+      </Fragment>
+    );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
