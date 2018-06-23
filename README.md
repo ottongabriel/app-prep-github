92de2cf27ce630f10890a1a46bcdb60ff5bebbfd
Ernesto Rodriguez <nestor@visionpoint.io>
2018-06-23 00:15:46 -0400

    Deleted files from boilerplate that students will start with

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# app-prep-github
+# GitHub Search, App Prep React Training
+
+After cloning the repo, run `npm install` followed by `npm start` to run the app.
--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import './App.css';
 
 class App extends Component {
   render() {
-    return <div className="App">Hey</div>;
+    return <div>I am ready to rock this training!</div>;
   }
 }
 
